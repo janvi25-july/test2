fc24d23cb5df82814ecc33823f96ebce3be8b719
janvi25-july <jahanvibhargav77@gmail.com>
2025-07-17 13:37:02 +0530

    Update Test2Application.java

--- test2/src/main/java/com/example/test2/Test2Application.java
+++ test2/src/main/java/com/example/test2/Test2Application.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 	 int y=200; 
          float a=10.3f;   
          double z =10.5; 
-          double y=55.5;    
+         int a= 500;    
 
        public static void main(String[] args) {
 	SpringApplication.run(Test2Application.class, args);
